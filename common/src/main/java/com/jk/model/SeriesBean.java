54d89ef8634a74fb1733b6704a660e9ec002ed9f
speng <18888381649@163.com>
2019-05-16 23:04:31 +0800

    第一次上传实体bean

---
+++ common/src/main/java/com/jk/model/SeriesBean.java
@@ -0,0 +1,22 @@
+package com.jk.model;
+
+public class SeriesBean {
+    private Integer seriesid;//车系id
+    private String seriesname;//车系name
+
+    public Integer getSeriesid() {
+        return seriesid;
+    }
+
+    public void setSeriesid(Integer seriesid) {
+        this.seriesid = seriesid;
+    }
+
+    public String getSeriesname() {
+        return seriesname;
+    }
+
+    public void setSeriesname(String seriesname) {
+        this.seriesname = seriesname;
+    }
+}
---
+++ common/src/main/java/com/jk/model/YeartypeBean.java
@@ -0,0 +1,22 @@
+package com.jk.model;
+
+public class YeartypeBean {
+    private Integer yeartypeid;
+    private String year;
+
+    public Integer getYeartypeid() {
+        return yeartypeid;
+    }
+
+    public void setYeartypeid(Integer yeartypeid) {
+        this.yeartypeid = yeartypeid;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
