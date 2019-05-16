80d06c3c2eb9cec11fa800695dd798e1163b0e26
chen276767590 <276767590@qq.com>
2019-05-16 21:58:22 +0800

    实体bean czy

---
+++ common/src/main/java/com/jk/model/SptimeBean.java
@@ -0,0 +1,24 @@
+package com.jk.model;
+
+import java.util.Date;
+
+public class SptimeBean {
+    private Integer sptimeid;
+    private Date sptime;
+
+    public Integer getSptimeid() {
+        return sptimeid;
+    }
+
+    public void setSptimeid(Integer sptimeid) {
+        this.sptimeid = sptimeid;
+    }
+
+    public Date getSptime() {
+        return sptime;
+    }
+
+    public void setSptime(Date sptime) {
+        this.sptime = sptime;
+    }
+}
