ba62da534ba879a12774f3039f2116b2155d0d9a
e <1009092747@qq.com>
2019-05-21 19:28:45 +0800

    新增完了

--- eureka/src/main/java/com/jk/EurekaApplication.java
+++ eureka/src/main/java/com/jk/EurekaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer//代表开启eureka的注册中服务
 public class EurekaApplication {
 
     public static void main(String[] args) {
